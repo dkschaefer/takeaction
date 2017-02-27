a1e886ea9996847ffb75642334cdfb8ad6005fa4
Derek Schaefer <schaefertek@gmail.com>
2017-02-27 15:28:12 -0500

    change default route / welcome page

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('petitionCRUD.index');
 });
 
 Route::resource('petitionCRUD','PetitionCRUDController');
