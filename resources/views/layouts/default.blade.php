2f7db6ef12a4d10e020808a2c2692c9416232ab6
Derek Schaefer <schaefertek@gmail.com>
2017-02-27 15:42:00 -0500

    abstracted inline styles and added to default blade template

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -8,6 +8,12 @@
     <title>TakeAction CRUD</title>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/css/bootstrap.css" rel="stylesheet">
 
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+    <!-- Global CSS Styles -->
+    <link rel="stylesheet" type="text/css" href="css/app.css">
+
 </head>
 <body>
  
